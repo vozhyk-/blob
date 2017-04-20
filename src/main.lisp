44a30ae006a1de63ffbeaf963b0072bd8de51def
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2017-04-20 08:57:42 +0200

    Print received messages

--- src/main.lisp
+++ src/main.lisp
@@ -7,7 +7,7 @@
     (let ((ws (make-server env)))
       (on :message ws
           (lambda (message)
-            (send ws message)))
+            (send ws (print message))))
       (lambda (responder)
         (declare (ignore responder))
         (start-connection ws)))))
