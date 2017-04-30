9b0eae44011877f23ef00d9f54911a2b004e76c4
Vitaut Bajaryn <vitaut.bayaryn@gmail.com>
2017-04-30 23:25:34 +0200

    Add simple bot logic

--- src/main.lisp
+++ src/main.lisp
@@ -9,9 +9,48 @@
   (with-output-to-string (stream)
     (yason:encode action stream)))
 
+(defun blob-position (blob)
+  (gethash "position" blob))
+
+(defun x (position)
+  (gethash "x" position))
+
+(defun y (position)
+  (gethash "y" position))
+
+(defun rad->deg (radians)
+  (* (/ radians pi) 180))
+
+(defun blob-direction (blob)
+  (let ((position (blob-position blob)))
+    (rad->deg (atan (y position)
+                    (x position)))))
+
+(defun blob-distance (blob)
+  (let* ((position (blob-position blob)))
+    (with-accessors ((x x) (y y)) position
+      (sqrt (* x x y y)))))
+
+(defun closest-blob (world)
+  (let ((sorted (sort world #'< :key #'blob-distance)))
+    (and sorted (first sorted))))
+
+(defun blob-type (blob)
+  (gethash "cellType" blob))
+
+(defun bot-direction (world)
+  (let ((closest-blob (closest-blob world)))
+    (if (null closest-blob)
+        45
+        (let ((direction (print (blob-direction closest-blob))))
+          (case (blob-type closest-blob)
+            ((0 2) (- direction))
+            (1 direction)
+            (t 45))))))
+
 (defun bot-action (world)
-  (declare (ignore world))
-  (alexandria:plist-hash-table '("direction" 45)))
+  (alexandria:plist-hash-table
+   (list "direction" (print (bot-direction world)))))
 
 (defun reply (message)
   (encode-action (bot-action (decode-world message))))
