57e7fe93fa56286016f9e81fc2836f8489c30b94
Aleksander Mistewicz <a.mistewicz@gmail.com>
2017-05-28 19:15:03 +0200

    Add more operators and literals with weigths
    It seems that two do not mix together

--- src/evolution.lisp
+++ src/evolution.lisp
@@ -2,11 +2,32 @@
 (deftype distance () 'real)
 
 (defparameter *operators* (list (mgl-gpr:operator (blob-direction blob)
-                                                  direction)
-                                (mgl-gpr:operator (blob-distance blob) distance)
-                                (mgl-gpr:operator (blob-type blob) type)))
+                                                  direction
+                                                  :weight 0.6)
+                                (mgl-gpr:operator (blob-distance blob) distance
+                                                  :weight 0.6)
+                                (mgl-gpr:operator (+ direction direction) direction
+                                                  :weight 0.05)
+                                (mgl-gpr:operator (+ distance distance) distance
+                                                  :weight 0.05)
+                                (mgl-gpr:operator (if condi
+                                                    direction
+                                                    direction) direction
+                                                  :weight 0.2)
+                                (mgl-gpr:operator (< distance distance) condi
+                                                  :weight 0.4)
+                                (mgl-gpr:operator (= type type) condi
+                                                  :weight 0.4)
+                                (mgl-gpr:operator (blob-type blob) type
+                                                  :weight 0.05)))
 (defparameter *literals* (list (mgl-gpr:literal (blob)
-                                 '(closest-blob *world*))))
+                                 '(closest-blob *world*))
+                               (mgl-gpr:literal (direction)
+                                 (random 360.0)
+                                 :weight 0.05)
+                               (mgl-gpr:literal (distance)
+                                 (random 100)
+                                 :weight 0.05)))
 
 (defvar *world*)
 (defvar *gp*
