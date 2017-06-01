5176a8339c0fea5eef6bcbdde256c6854e71cb9b
Aleksander Mistewicz <mistewicza@student.mini.pw.edu.pl>
2017-06-01 10:55:04 +0200

    Separate types used in literals and operators

--- src/evolution.lisp
+++ src/evolution.lisp
@@ -1,50 +1,40 @@
-(deftype direction () '(real 0 360))
-(deftype distance () 'real)
-
 (defparameter *operators* (list ;; Blob related operators
-                                (mgl-gpr:operator (blob-direction blob) direction
-                                                  :weight 0.2)
-                                (mgl-gpr:operator (blob-distance blob) distance
-                                                  :weight 0.2)
-                                (mgl-gpr:operator (blob-type blob) typei
-                                                  :weight 0.2)
+                                (mgl-gpr:operator (blob-direction blob) direction)
+                                (mgl-gpr:operator (blob-distance blob) distance)
+                                (mgl-gpr:operator (blob-type blob) type)
                                 ;; Branch
-                                (mgl-gpr:operator (if condi
+                                (mgl-gpr:operator (if cond
                                                     direction
                                                     direction) direction
+                                                  :weight 0.5)
+                                (mgl-gpr:operator (and cond cond) cond
+                                                  :weight 0.01)
+                                (mgl-gpr:operator (or cond cond) cond
+                                                  :weight 0.01)
+                                ;; Direction-related condtions
+                                (mgl-gpr:operator (< direction direction) cond
+                                                  :weight 0.1)
+                                ;; Distance-related condtions
+                                (mgl-gpr:operator (< distance distance) cond
                                                   :weight 0.2)
-                                ;; Direction-related conditions
-                                (mgl-gpr:operator (< direction direction) condi
+                                (mgl-gpr:operator (< const-distance distance) cond
                                                   :weight 0.1)
-                                (mgl-gpr:operator (and condi condi) condi
+                                (mgl-gpr:operator (< distance const-distance) cond
                                                   :weight 0.1)
-                                ;; Distance-related conditions
-                                (mgl-gpr:operator (< distance distance) condi
+                                ;; Type-related condtions
+                                (mgl-gpr:operator (= type type) cond
                                                   :weight 0.1)
-                                (mgl-gpr:operator (< distance distance) condi
-                                                  :weight 0.1)
-                                ;; Type-related conditions
-                                (mgl-gpr:operator (= typei typei) condi
-                                                  :weight 0.1)))
+                                (mgl-gpr:operator (= const-type type) cond
+                                                  :weight 0.3)))
 (defparameter *literals* (list (mgl-gpr:literal (blob)
                                  '(closest-blob *world*))
-                               (mgl-gpr:literal (direction)
+                               (mgl-gpr:literal (const-direction)
                                  (random 360.0))
-                               (mgl-gpr:literal (distance)
+                               (mgl-gpr:literal (const-distance)
                                  (random 100))
-                               (mgl-gpr:literal (typei)
-                                                :weight 0.1
-                                                0)
-                               (mgl-gpr:literal (typei)
-                                                :weight 0.1
-                                                1)
-                               (mgl-gpr:literal (typei)
-                                                :weight 0.1
-                                                2)
-                               (mgl-gpr:literal (condi)
-                                                t)
-                               (mgl-gpr:literal (direction-cond)
-                                                nil)))
+                               (mgl-gpr:literal (cosnt-type) 0)
+                               (mgl-gpr:literal (cosnt-type) 1)
+                               (mgl-gpr:literal (const-type) 2)))
 
 (defvar *world*)
 (defvar *gp*
